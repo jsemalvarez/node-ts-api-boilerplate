3d199df8836bb646b6d4ca3c84d0917f810f0f00
jsemalvarez <jsemalvarez@gmail.com>
2024-04-05 23:20:40 -0300

    fix: admin routes

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import userRoutes from './user.routes';
 import toosRoutes from './todo.routes';
+import adminRoutes from './admin.routes';
 
 const router = Router();
 
 router.use('/users', userRoutes);
 router.use('/todos', toosRoutes);
+router.use('/admin', adminRoutes);
 
 router.get('/health', (_req, res) => res.json({ message: 'App is OK :)' }));
 
