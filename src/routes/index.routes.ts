66a43cf0488f13b36817f54b9f9c6e40cf8a5cd6
jsemalvarez <jsemalvarez@gmail.com>
2024-04-05 23:21:29 -0300

    fix: todos routes

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import userRoutes from './user.routes';
-import toosRoutes from './todo.routes';
+import todosRoutes from './todo.routes';
 import adminRoutes from './admin.routes';
 
 const router = Router();
 
 router.use('/users', userRoutes);
-router.use('/todos', toosRoutes);
+router.use('/todos', todosRoutes);
 router.use('/admin', adminRoutes);
 
 router.get('/health', (_req, res) => res.json({ message: 'App is OK :)' }));
--- src/routes/todo.routes.ts
+++ src/routes/todo.routes.ts
@@ -10,15 +10,15 @@ router.get('/', (_req, res: Response) => {
   res.json({ message: 'endpoint get-all todos' });
 });
 
-router.get('/todoId', (_req, res: Response) => {
+router.get('/:todoId', (_req, res: Response) => {
   res.json({ message: 'endpoit get-todo by id' });
 });
 
-router.patch('/todoId', (_req, res: Response) => {
+router.patch('/:todoId', (_req, res: Response) => {
   res.json({ message: 'endpoit update-todo by id' });
 });
 
-router.delete('/todoId', (_req, res: Response) => {
+router.delete('/:todoId', (_req, res: Response) => {
   res.json({ message: 'endpoit delete-todo by id' });
 });
 
