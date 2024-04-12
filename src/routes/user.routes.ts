3dc1d359dc6fd776c9829644de9dc62d2f0f5718
jsemalvarez <jsemalvarez@gmail.com>
2024-04-12 17:12:34 -0300

    feat: validate data with middlewares

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,22 +2,26 @@ import { Router } from 'express';
 
 import * as userContrller from '../controllers/user.controllers';
 import { validateDataMiddleware, validateTokenMiddleware, validateRoleMiddleware } from '../middlewares';
-import { updateUser } from '../middlewares/validations/user.validations';
+import { createUser, getUser, loginUser, removeUser, updateUser } from '../middlewares/validations/user.validations';
 import { UserRole } from '../interfaces/user.interface';
 
 const router = Router();
 
-router.post('/register', userContrller.register);
+router.post('/register', [validateDataMiddleware(createUser)], userContrller.register);
 
 router.get('/', [validateTokenMiddleware, validateRoleMiddleware(UserRole.ADMIN)], userContrller.findAll);
 
-router.get('/:userId', [validateTokenMiddleware, validateRoleMiddleware(UserRole.ADMIN)], userContrller.findOne);
+router.get('/:userId', [validateDataMiddleware(getUser), validateTokenMiddleware], userContrller.findOne);
 
 router.patch('/:userId', [validateDataMiddleware(updateUser), validateTokenMiddleware], userContrller.update);
 
-router.delete('/:userId', [validateTokenMiddleware, validateRoleMiddleware(UserRole.ADMIN)], userContrller.remove);
+router.delete(
+  '/:userId',
+  [validateDataMiddleware(removeUser), validateTokenMiddleware, validateRoleMiddleware(UserRole.ADMIN)],
+  userContrller.remove,
+);
 
-router.post('/login', userContrller.login);
+router.post('/login', [validateDataMiddleware(loginUser)], userContrller.login);
 
 router.post('/refresh-token', userContrller.refreshToken);
 
