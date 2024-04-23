7e22df325bce27e1d66f0b85b4fa9d9993edba11
jsemalvarez <jsemalvarez@gmail.com>
2024-04-23 20:57:35 -0300

    feat: implements custom logger in error.middleware.ts

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,10 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 import { ErrorI } from '../interfaces';
+import { loggerAdapter } from '../utils/logger.adapter';
 
 // eslint-disable-next-line
 export const errorHandler = (err: ErrorI.CustomError, req: Request, res: Response, _next: NextFunction) => {
-  console.error(`Error en la ruta ${req.method} ${req.url}: ${err.stack || err.message}`);
   const statusCode = err.statusCode || 500;
   const message = statusCode < 500 ? err.message : 'Server error';
+
+  const origin = err.origin || err.stack || 'unknown';
+  loggerAdapter.error(`Error en la ruta ${req.method} ${req.url}: ${err.message}`, origin);
+
   res.status(statusCode).json({ message });
 };
